877bbb10174cb2ee9dc5597eb94e2aac099da5b5
אלכס לויטין <alexande.levitin@ruppin365.net>
2022-12-24 18:15:15 +0200

    Android Unit Converter - added lbs to kg conversion

--- app/src/main/java/com/example/androidunitconverter/WeightConversion.java
+++ app/src/main/java/com/example/androidunitconverter/WeightConversion.java
@@ -10,6 +10,8 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +22,9 @@ public class WeightConversion extends AppCompatActivity {
     EditText inputWeight;
     Button convertWeight;
     TextView viewRes;
+    RadioGroup radioGroup;
+    RadioButton radioKG;
+    RadioButton radioLBS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +33,27 @@ public class WeightConversion extends AppCompatActivity {
         inputWeight = (EditText)findViewById(R.id.WeightInput);
         convertWeight = findViewById(R.id.ConvertWeight);
         viewRes = findViewById(R.id.Res);
+        radioGroup = findViewById(R.id.Radio_Group_Weight);
+        radioKG = findViewById(R.id.Radio_Button_KG);
+        radioLBS = findViewById(R.id.Radio_Button_LBS);
 
 
         convertWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
+                int checkedId = radioGroup.getCheckedRadioButtonId();
                 String input = inputWeight.getText().toString();
                 if(input.equals("")) {
                     inputWeight.setError("Please Enter Weight");
                     Toast.makeText(WeightConversion.this, "Please Enter Weight", Toast.LENGTH_SHORT).show();
                     viewRes.setText("");
                 }
+                else if(checkedId == radioKG.getId())
+                    viewRes.setText("Value in LBS is: " + new DecimalFormat("##.##").format(ConversionKgtoLbs(inputWeight)));
                 else
-                    viewRes.setText("Value in LBS is: " + new DecimalFormat("##.##").format(Conversion(inputWeight)));
+                    viewRes.setText("Value in KG is: " + new DecimalFormat("##.##").format(ConversionLbstoKg(inputWeight)));
+
             }
         });
 
@@ -49,12 +61,18 @@ public class WeightConversion extends AppCompatActivity {
 
 
     }
-    public Double Conversion(@NonNull EditText userInput)
+    public Double ConversionKgtoLbs(@NonNull EditText userInput)
     {
         Double x = Double.parseDouble(userInput.getText().toString());
         // if KG --> LBS
         x =  x * 2.20462;
         return x;
     }
-
+    public Double ConversionLbstoKg(@NonNull EditText userInput)
+    {
+        Double x = Double.parseDouble(userInput.getText().toString());
+        // if KG --> LBS
+        x =  x / 2.20462;
+        return x;
+    }
 }
